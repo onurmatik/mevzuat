ac662536be8bfaf32f5fbbdba8bbb5fae22a915f
onur mat <onurmatik@gmail.com>
2025-08-16 16:19:37 +0300

    Added RDS settings

--- mevzuat/settings.py
+++ mevzuat/settings.py
@@ -69,12 +69,26 @@ WSGI_APPLICATION = 'mevzuat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+RDS_HOST = os.getenv('RDS_HOST')
+if RDS_HOST:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'HOST': RDS_HOST,
+            'PORT': '5432',
+            'NAME': os.getenv("RDS_NAME"),
+            'USER': os.getenv("RDS_USER"),
+            'PASSWORD': os.getenv("RDS_PASSWORD"),
+        },
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 # Password validation
