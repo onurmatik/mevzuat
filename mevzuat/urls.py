a739da33ab6acc0e676534840550bce4d4c29e24
onur mat <onurmatik@gmail.com>
2025-08-20 21:08:24 +0300

    Minor UI updates

--- mevzuat/urls.py
+++ mevzuat/urls.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-from django.urls import path, re_path
-from django.conf import settings
-from django.views.static import serve as static_serve
-from pathlib import Path
+from django.urls import path
 from ninja import NinjaAPI
 from mevzuat.documents.api import router as documents_router
 from mevzuat.documents import views as documents_views
@@ -13,9 +10,12 @@ api.add_router("/documents", documents_router)
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('mAdmin/', admin.site.urls),
     path("api/", api.urls),
     path("", documents_views.main, name="home"),
     path("search/", documents_views.search, name="search"),
 ]
 
+admin.site.index_title = 'Welcome to the Mevzuat Administration'
+admin.site.site_header = 'Mevzuat Administration'
+admin.site.site_title = 'Mevzuat Administration'
