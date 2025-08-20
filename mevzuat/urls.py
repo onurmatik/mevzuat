88fe0d0788600edba481f969a34b99195a4db8d0
onur mat <onurmatik@gmail.com>
2025-08-20 10:48:46 +0300

    Homepage update

--- mevzuat/urls.py
+++ mevzuat/urls.py
@@ -19,19 +19,3 @@ urlpatterns = [
     path("search/", documents_views.search, name="search"),
 ]
 
-
-if settings.DEBUG:
-    # Serve next.js static export in local development
-    FRONTEND_OUT = Path(settings.BASE_DIR) / "frontend" / "out"
-
-    # Serve Next.js assets under /_next/*
-    urlpatterns += [
-        re_path(r"^_next/(?P<path>.*)$",
-                static_serve, {"document_root": FRONTEND_OUT / "_next"}),
-    ]
-
-    # Catch-all: serve exported HTML (index.html, other pages)
-    urlpatterns += [
-        re_path(r"^(?P<path>.*)$",
-                static_serve, {"document_root": FRONTEND_OUT, "show_indexes": False}),
-    ]
