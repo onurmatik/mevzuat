95ae13a33c25848c38344bc4f7fbeb76f1738041
onur mat <onurmatik@gmail.com>
2025-09-03 22:36:53 +0300

    Order by date

--- mevzuat/documents/views.py
+++ mevzuat/documents/views.py
@@ -4,7 +4,7 @@ from .models import Document
 
 
 def main(request):
-    recent_documents = Document.objects.order_by("-created_at")[:5]
+    recent_documents = Document.objects.order_by("-date")[:5]
     return render(request, "home.html", {"recent_documents": recent_documents})
 
 
